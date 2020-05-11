4f1dd36c1befdf52f513f74ab53609ca95e42a41
Plamen Totev <plamen.totev@icb.bg>
2020-05-11 23:45:39 +0300

    Initial commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback)=>{
             callback('Unable to find location')
     
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is ' + body.current.temperature + ' deg and there it feels like ' + body.current.feelslike + '.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is ' + body.current.temperature + ' deg and there it feels like ' + body.current.feelslike + '. Humidity is ' + body.current.humidity)
         }
     })
 }
